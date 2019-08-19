b88db697d34c74b26c25c9622ce5503f892dd01c
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-08-19 09:20:22 +0000

    [Studio] Add screen "HomeIconColorBlocks"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,19 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
 import { blank_PhonePortrait } from '../pages/blank/PhonePortrait/blank';
+import { homeiconcolorblocks_PhonePortrait } from '../pages/homeiconcolorblocks/PhonePortrait/homeiconcolorblocks';
 export class Screens {
   static declarations = [
     MenuComponent,
-    blank_PhonePortrait
+    blank_PhonePortrait,
+    homeiconcolorblocks_PhonePortrait
   ];
   static mapping = {
     'blank': {
       PhonePortrait: blank_PhonePortrait
+    },
+    'homeiconcolorblocks': {
+      PhonePortrait: homeiconcolorblocks_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,2 +1,3 @@
 export { mock_blank as blank } from './blank/index';
+export { mock_homeiconcolorblocks as homeiconcolorblocks } from './homeiconcolorblocks/index';
 export { mock_menu as menu } from './menu/index';
